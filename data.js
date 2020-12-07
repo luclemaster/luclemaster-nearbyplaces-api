ed55792572761901c423b5913a0dcde5053e290d
luclemaster <llemaster@email.arizona.edu>
2020-12-07 15:32:05 -0700

    fixed naming

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 let places = [{name: 'a', city: 'Tucson', state: 'Arizona', reviews: []},
 {name: 'b', city: 'carlos', state: 'Arizona', reviews: []},
 {name: 'c', city: 'Tucson', state: 'phoenix', reviews: []},
-{name: 'a', city: 'Tucson', state: 'Arizona', reviews: []}
+{name: 'd', city: 'Tucson', state: 'Arizona', reviews: []}
 ]
 
 module.exports.places = places;
