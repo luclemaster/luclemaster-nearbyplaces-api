52ea02c0e1d10876b776d83586c3814e817e9b5d
luclemaster <llemaster@email.arizona.edu>
2020-12-08 15:58:52 -0700

    small change

--- db.js
+++ db.js
@@ -21,6 +21,6 @@ function savePlace(name, city, state, description) {
     return postgrePool.query('INSERT INTO nearbyplaces.places ("name", city, state, description, reviews) VALUES ($1, $2, $3, $4, $5)', [name, city, state, description, ""]); 
 }
 function deletePlace(name) {
-    return postgrePool.query(`DELETE FROM nearbyplaces.places WHERE name =${name}`)
+    return postgrePool.query(`DELETE FROM nearbyplaces.places WHERE "name"=${name}`)
 }
 module.exports = {getPlaces, savePlace, deletePlace}
