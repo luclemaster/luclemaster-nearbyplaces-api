49f58d74b4a45a246c288f47b11f348339bea513
luclemaster <llemaster@email.arizona.edu>
2020-12-09 13:50:05 -0700

    added city state

--- db.js
+++ db.js
@@ -33,6 +33,6 @@ function getReview(name) {
     return postgrePool.query('SELECT reviews FROM nearbyplaces.places WHERE "name"= $1', [name]).then(x => x.rows);
 }
 function searchLocation(city, state, searchTerm) {
-    postgrePool.query(`SELECT * FROM nearbyplaces.place WHERE city='${city}' AND state='${state}'`).then(x => x.rows);
+    postgrePool.query('SELECT * FROM nearbyplaces.place WHERE city=$1 AND state=$2', [city, state]).then(x => x.rows);
 }
 module.exports = {getPlaces, savePlace, deletePlace,getPlace, placeReview, getReview, searchLocation}
