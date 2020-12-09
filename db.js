6143245bf142d6fd06b9af21812fe287400f4869
luclemaster <llemaster@email.arizona.edu>
2020-12-08 20:44:40 -0700

    fiexed typo

--- db.js
+++ db.js
@@ -21,6 +21,6 @@ function savePlace(name, city, state, description) {
     return postgrePool.query('INSERT INTO nearbyplaces.places ("name", city, state, description, reviews) VALUES ($1, $2, $3, $4, $5)', [name, city, state, description, ""]); 
 }
 function deletePlace(name) {
-    return postgrePool.query('DELETE FROM nearbyplaces.places WHERE "name"= $1',+ [name]);
+    return postgrePool.query('DELETE FROM nearbyplaces.places WHERE "name"= $1', [name]);
 }
 module.exports = {getPlaces, savePlace, deletePlace}
