866c11cbd678d7c2c5dcd616d53d24e143e53a57
luclemaster <llemaster@email.arizona.edu>
2020-12-09 10:24:45 -0700

    testing review

--- server.js
+++ server.js
@@ -38,14 +38,14 @@ app.get('/delete/:name', (request, response) => {
 app.post('/review/:placeId' , (request, response) => {
     // add functionality to combine old and new review
     let review = db.getReview(request.params.placeId)
-    response.json(review);
-    let user_review = request.body.review;
-    //if(review.length >0) {
-    //    review += ', ' + 
-    //} else {
-    //    review = request.body.review
-   // }
-    db.placeReview(name, review);
+    
+    if(review.length >0) {
+        review += ', ' + request.body.review
+    } else {
+        review = request.body.review
+    }
+   response.json(review);
+    //db.placeReview(name, review);
 });
 app.get('/search/:searchTerm/:location', (request, response) => {
     let [city, state] = request.params.location.split(',');
