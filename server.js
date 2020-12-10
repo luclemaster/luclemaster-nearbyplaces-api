03d7d101fa9cb7f27f3f3f86f86f0034bcc99e30
luclemaster <llemaster@email.arizona.edu>
2020-12-09 22:05:35 -0700

    test

--- server.js
+++ server.js
@@ -38,8 +38,7 @@ app.get('/delete/:name', (request, response) => {
 app.get('/review/:placeId/' , (request, response) => {
     // add functionality to combine old and new review
     db.getReview(request.params.placeId).then(y => {
-        let review = y[0].reviews;
-        response.send(review);
+        response.send(y[0].reviews);
         /*
         if(review.length > 0){
             review += ', ' + request.body.review;
